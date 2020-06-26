8f9ebe7fbdbaef827225848c125386f15396c66a
jorishr <joris@jorisr.com>
2020-06-26 17:38:25 +0200

    query parameters and pagination tests added

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 
 //enable sending json to the server
 app.use(bodyParser.json());
+//enable sending url-encoded formdata to the server
+app.use(express.urlencoded({ extended: true }));
 
 //routes
 app.get('/', (req, res) => {
@@ -22,10 +24,12 @@ app.delete('/', (req, res) => {
     const foo = 0;
     res.status(202).json({ foo: foo });
 })
+
 //error page
 app.get('/error', (req, res) => {
     res.status(500).end();
 })
+
 //wrong method used (everything but GET)
 app.all('/error', (req, res) => {
     res.status(405).end();
@@ -36,12 +40,58 @@ app.get('/auth', (req, res) => {
     res.status(401).end();
     //res.status(403).end();
 })
+
 //bad request (wrong username or password)
-app.get('/login', (req, res) => {
+app.get('/failed-login', (req, res) => {
     res.status(400).end();
 })
+
 //user redirect
 app.get('/redirect', (req, res) => {
     res.status(302).redirect('/');
 })
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    if(username === 'foo' && password === 'bar'){
+        res.status(200).end();
+    }
+    res.status(400).end();
+})
+
+app.get('/query', (req, res) => {
+    const { firstParam, secondParam } = req.query;
+    if(firstParam === 'foo' && secondParam === 'bar'){
+        res.status(200).end();
+    }
+    res.status(400).end();
+})
+
+//pagination with query parameters
+const users = {
+    john: {
+        name: 'john'
+    },
+    jane: {
+        name: 'jane'
+    },
+    bob: {
+        name: 'bob'
+    }
+}
+
+app.get('/users', (req, res) => {
+    //query values are strings, thus convert to integer
+    const limitInt = parseInt(req.query.limit);
+    const offsetInt = parseInt(req.query.offset);
+    //no limit, send all users
+    const length = limitInt > 0 ? limitInt : users.length;
+    const offset = offsetInt > 0 ? offsetInt : 0;
+    //result should be array
+    const result = Object.values(users)
+        .slice(offset)      //only keep from offset index to the end
+        .slice(0, length);  //apply limit
+    res.json(result);
+})
+
 module.exports = app;
--- src/app.test.js
+++ src/app.test.js
@@ -23,28 +23,41 @@ describe('Get JSON', () => {
         expect(actual).toEqual(expected);
     })
 })
+
 describe('Update JSON', () => {
     it('Can Put /', async () => {
         await request(app).put('/').expect(200);
     })
     it('Should return two foo', async () => {
-        const response = await request(app).put('/').expect(200).send({ foo : 1 });
+        const response = 
+            await request(app)
+                .put('/')
+                .set('Content-Type', 'application/json')
+                .expect(200)
+                .send({ foo : 1 });
         const expected = { foo: 2 };
         const actual = response.body;
         expect(actual).toEqual(expected);
     })
 })
+
 describe('Post JSON', () => {
     it('Can Post /', async () => {
         await request(app).post('/').expect(201);
     })
     it('Should return one foo', async () => {
-        const response = await request(app).post('/').expect(201).send({ foo : 1 });
+        const response = 
+            await request(app)
+                .post('/')
+                .set('Content-Type', 'application/json')
+                .expect(201)
+                .send({ foo : 1 });
         const expected = { foo: 1 };
         const actual = response.body;
         expect(actual).toEqual(expected);
     })
 })
+
 describe('Delete JSON', () => {
     it('Can Delete /', async () => {
         await request(app).delete('/').expect(202);
@@ -56,16 +69,82 @@ describe('Delete JSON', () => {
         expect(actual).toEqual(expected);
     })
 })
+
 describe('Authorized access', () => {
     it('Unauthorized request returns status code 401', async () => {
         await request(app).get('/auth').expect(401);
     })
     it('Bad user request returns status code 400', async () => {
-        await request(app).get('/login').expect(400);
+        await request(app).get('/failed-login').expect(400);
     })
 })
+
 describe('Redirect', () => {
     it('Returns status code 302', async () => {
         await request(app).get('/redirect').expect(302);
+    })
+})
+
+describe('Send form login data', () => {
+    it('Returns status code 200 for correct credentials', async () => {
+        await request(app)
+            .post('/login')
+            .set('Content-Type','application/x-www-form-urlencoded')
+            .expect(200)
+            .send({ username: 'foo', password: 'bar'})
+        })
+        it('Returns status code 400 for incorrect credentials', async () => {
+        await request(app)
+        .post('/login')
+        .set('Content-Type','application/x-www-form-urlencoded')
+        .expect(400)
+            .send({ username: 'foos', password: 'bar'})
+        })
+        it('Returns status code 400 for incorrect credentials', async () => {
+        await request(app)
+        .post('/login')
+            .set('Content-Type','application/x-www-form-urlencoded')
+            .expect(400)
+            .send({ username: 'foo', password: 'bars'})
+    })
+})
+
+describe('Query parameter tests', () => {
+    it('Returns status code 200 if params are valid', async () => {
+        await request(app)
+            .get('/query/?firstParam=foo&secondParam=bar')
+            .expect(200);
+    })
+    it('Returns status code 400 if params are invalid', async () => {
+        await request(app)
+            .get('/query/?firstParam=oo&secondParam=bar')
+            .expect(400);
+    })
+    it('Returns status code 400 if params are invalid', async () => {
+        await request(app)
+            .get('/query/?firstParam=foo&secondParam=ar')
+            .expect(400);
+    })
+})
+describe('Pagination tests', () => {
+    it('Returns a list of users with correct length', async () => {
+        const response = await request(app)
+            .get('/users/?limit=2&offset=1')
+            .expect(200);
+        const body = response.body;
+        expect(Array.isArray(body)).toBe(true);
+        expect(body.length).toBe(2);
+        expect(body[0].name).toEqual('jane');
+    })
+    it('Returns a list of users with correct length', async () => {
+        const response = await request(app)
+            .get('/users/?limit=0&offset=0')
+            .expect(200);
+        const body = response.body;
+        expect(Array.isArray(body)).toBe(true);
+        expect(body.length).toBe(3);
+        expect(body[0].name).toEqual('john');
+        expect(body[1].name).toEqual('jane');
+        expect(body[2].name).toEqual('bob');
     })
 })
